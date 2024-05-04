4ba05a648ce6d2187542ad23501c6cb2e471d347
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2024-05-05 01:36:07 +0530

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+    <h1 className="bg-blue-500">hI</h1>
+    </main>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+const Header = () => {
+  return (
+    <>
+    <Link href="/login"><h1>Sign in</h1></Link>
+    </>
+  )
+}
+
+export default Header
