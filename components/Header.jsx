ced27a49744e3824376adba8549fd7235e2d5bde
ShivanshAr97 <shivansh.arora973@gmail.com>
2024-08-30 01:46:45 +0530

    added base components

---
+++ components/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Logout from "./Buttons/Logout";
+
+const Header = async () => {
+  const session = await getServerSession(authOptions);
+  return (
+    <header className="bg-white shadow-sm py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <div className="text-2xl font-bold text-gray-800">
+          <Link href={"/"}>LinkFolio</Link>
+        </div>
+        <nav className="flex space-x-6 text-gray-600">
+          <Link href={"/about"} className="hover:text-gray-900 transition duration-300">About</Link>
+          <Link href={"/contact"} className="hover:text-gray-900 transition duration-300">Contact</Link>
+          {!session ? (
+            <>
+              <Link href={"/login"} className="hover:text-gray-900 transition duration-300">Sign In</Link>
+              <Link href={"/register"} className="hover:text-gray-900 transition duration-300">Create account</Link>
+            </>
+          ) : (
+            <>
+              <Link href={"/account"} className="hover:text-gray-900 transition duration-300">
+                Hello, {session?.user?.name}
+              </Link>
+              <Logout />
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
